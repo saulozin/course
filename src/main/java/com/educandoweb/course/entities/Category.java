cf73f8ae5d5b1e6754f196f264add401ccf6d3a8
Saulo Maia <saulomaia2@gmail.com>
2023-03-01 15:05:27 -0400

    Delete Exception

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -19,6 +19,7 @@ import jakarta.persistence.Table;
 public class Category implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -22,6 +22,7 @@ import jakarta.persistence.Table;
 public class Product implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
